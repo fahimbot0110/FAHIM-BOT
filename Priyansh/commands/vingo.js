355ca4b1a0729a7472e91b86c3c41b314906103b
fahim017272 <botfahim232@gmail.com>
2025-05-27 08:45:41 +0600

    Create vingo.js

---
+++ Priyansh/commands/vingo.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const fs = require('fs');
+
+module.exports.config = {
+    name: "sexvideo", 
+    version: "1.0.0", 
+    permssion: 0,
+    premium: false,
+    prefix: true,
+    credits: "FAHIM",
+    description: "SOME SEX VIDEO BY X2 SERVER",
+    category: "VIDEO", 
+    usages: "/videosex",
+    cooldowns: 5
+};
+
+module.exports.run = async ({ api, event, args }) => {
+    try {
+      const rt = await axios.get("https://x2-video-api.onrender.com/videosex")
+      const url = rt.data.resultX2.data;
+        const response = await axios.get(url, { responseType: "stream" });
+
+        const videoPath = './sex_video.mp4';
+        const writer = fs.createWriteStream(videoPath);
+
+        response.data.pipe(writer);
+
+        writer.on('finish', () => {
+            api.sendMessage({ body: "🥵-❊ 𝗙𝗔𝗛𝗜𝗠 𝗖𝗛𝗢𝗪𝗗𝗛𝗨𝗥𝗬 ❊-🥵", attachment: fs.createReadStream(videoPath) }, event.threadID);
+        });
+
+        writer.on('error', (err) => {
+            console.error(err);
+        });
+    } catch (e) {
+        console.log(e);
+    }
+};
